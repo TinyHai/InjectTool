c31948a8b47a9cb91da8c810e94bed5d37ecccd3
TinyHai <1035908357@qq.com>
2024-05-10 18:22:30 +0800

    refine build

--- build.sh
+++ build.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-cross build --target aarch64-linux-android --release
-cross build --target armv7-linux-androideabi --release
-cross build --target i686-linux-android --release
-cross build --target x86_64-linux-android --release
+declare -A MAP=(
+  ['aarch64-linux-android']='arm64-v8a'
+  ['armv7-linux-androideabi']='armeabi-v7a'
+  ['i686-linux-android']='x86'
+  ['x86_64-linux-android']='x86_64'
+)
+
+set -x
+if [[ -d out ]]; then
+  rm -rf out
+fi
+
+mkdir out
+
+for abi in "${MAP[@]}"
+do
+  mkdir out/$abi
+done
+
+for target in "${!MAP[@]}"
+do
+  cross build --target $target --release
+  cp target/$target/release/inject_tool out/${MAP[$target]}/libinject_tool.so
+done
+
+set +x
